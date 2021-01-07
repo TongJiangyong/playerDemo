27e8585ec54d63522bf4cb0741ff11c68d0d4b6d
TongJiangyong <1246376353@qq.com>
2021-01-07 20:03:05 +0800

    init android

---
+++ android/zego/common/src/main/java/im/zego/common/GetAppIDConfig.java
@@ -0,0 +1,14 @@
+package im.zego.common;
+
+public class GetAppIDConfig {
+    // Apply AppID and AppSign from Zego
+    // https://console-express.zego.im/account/login
+
+    // for example:
+    //     public static final long appID = 123456789L;
+    public static final long appID = 1098290204L;
+
+    // for example:
+    //    public static final String appSign = "0123456789012345678901234567890123456789012345678901234567890123";
+    public static final String appSign = "d0d4b79a8bdc664872662213130d6986204abc31093bc5186b3a2d1c0fe69a21";
+}
